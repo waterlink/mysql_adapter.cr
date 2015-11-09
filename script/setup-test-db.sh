f8d6299efc4740fa8a7fe89b0b266c87eb8a92f5
Oleksii Fedorov <waterlink000@gmail.com>
2015-11-09 03:00:42 +0100

    Upgrade to active_record 0.1.2

--- script/setup-test-db.sh
+++ script/setup-test-db.sh
@@ -8,7 +8,7 @@ mysql $PARAMS -e "create database crystal_mysql_test"
 mysql $PARAMS -e "create user 'crystal_mysql'@'localhost'"
 mysql $PARAMS -e "grant all on crystal_mysql_test.* to 'crystal_mysql'@'localhost'"
 
-mysql $PARAMS -e "use crystal_mysql_test; drop table if exists people; create table people( id int not null auto_increment primary key, last_name varchar(50), first_name varchar(50), number_of_dependents int )"
+mysql $PARAMS -e "use crystal_mysql_test; drop table if exists people; create table people( id int not null auto_increment primary key, last_name varchar(50), first_name varchar(50), number_of_dependents int, special_tax_group boolean )"
 
 mysql $PARAMS -e "use crystal_mysql_test; drop table if exists something_else; create table something_else( id int not null auto_increment primary key, name varchar(50) )"
 
